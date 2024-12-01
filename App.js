d7e8122b105f9d0e6b80af6815365de5a861740d
quantumgravitas <mayurgodase26@gmail.com>
2024-12-01 14:27:46 +0530

    created h1 element and rendered it inside the div root tag using react

--- App.js
+++ App.js
@@ -1,5 +1,40 @@
-const heading=React.createElement('h1',{},"Hello World From React!");
+/*const heading=React.createElement('h1',{id:'heading',xyz:"abc"},"Hello World From React!");
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(heading);
+
+console.log(heading);
+console.log(typeof heading);
+console.log(root);
+console.log(typeof root);
+*/
+/*
+<div id='parent'>
+  <div id="child1">
+    <h1>I'm  h1 tag</h1>
+    <h2>I'm  h2 tag</h2>
+  </div>
+  <div id="child2">
+    <h1>I'm  h1 tag</h1>
+    <h2>I'm h2 tag</h2>
+  </div>
+</div>
+*/
+
+const parent=React.createElement('div',{id:"parent"},[
+ React.createElement("div",{id:"child1"},[
+    React.createElement("h1",{},"I'm h1 tag"),
+    React.createElement("h2",{},"I'm h2 tag")
+ ]),
+  
+  React.createElement("div",{id:"child2"},[
+    React.createElement("h1",{},"I'm h1 tag"),
+    React.createElement("h2",{},"I'm h2 tag")
+ ])
+])
+  
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
