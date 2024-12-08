249afb41e589c27ceb31355e37fa1e7e2080d9fa
quantumgravitas <mayurgodase26@gmail.com>
2024-12-08 16:26:46 +0530

    toggle login/logout button functionality implemented using useState concept

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { LOGO_URL } from "../../utils/constants";
-import Body from "./Body";
+import { useState } from "react";
 const Header=()=>{
+  const [btnName,setBtnName]=useState("Login");
   return(
     <div className="header">
         <div className="logo-container">
@@ -12,6 +13,7 @@ const Header=()=>{
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Cart</li>
+            <button className="login-button" onClick={()=>{btnName==='Login'?setBtnName("Logout"):setBtnName('Login')}}>{btnName}</button>
           </ul>
         </div>
     </div>
